3bffc5899c7f7dd74cb193ec5fcadabcebe2c60b
Karol Stępień <karol.stepien@me.com>
2019-04-28 00:32:34 +0200

    Workbox implementation, cleanup, css loading improvement

--- config/webpack.main.js
+++ config/webpack.main.js
@@ -3,6 +3,9 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const autoprefixer = require('autoprefixer');
 
 module.exports = {
 
@@ -46,13 +49,30 @@ module.exports = {
                 }
             },
             {
-                test: /\.scss$/,
-                exclude: /node_modules/,
-                use: ['style-loader', 'css-loader', { loader: 'sass-loader', options: { importLoaders: 1 } },  "postcss-loader"]
-            },
-            {
-                test: /\.css$/,
-                loader: 'style-loader!css-loader'
+                test: /\.(scss|css)$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: "css-loader",
+                        options: {
+
+                        }
+                    },
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            autoprefixer: {
+                                browsers: ["last 2 versions"]
+                            },
+                            plugins: () => [
+                                autoprefixer
+                            ]
+                        },
+                    },
+                    {
+                        loader: "sass-loader"
+                    }
+                ]
             },
             {
                 test: /\.(png|jpg|gif|svg|otf)$/i,
@@ -76,22 +96,34 @@ module.exports = {
             to: './assets/',
         },
         {
-            from: './scripts/workers/',
-            to: './',
-        },
-        {
             from: './other/',
             to: './',
         }
-    ]),
+        ]),
         new HtmlWebpackPlugin({
-            template: 'index.html',
-            inject: true
+            filename: 'index.html',
+            template: 'index.html'
         }),
-        new HtmlWebpackPlugin({
-            template: '404.html',
-            filename: '404.html',
-            inject: true
-        })
+        new MiniCssExtractPlugin({
+        }),
+        new WorkboxPlugin.GenerateSW({
+            clientsClaim: true,
+            skipWaiting: true,
+            exclude: [/\.(?:png|jpg|jpeg|svg)$/],
+            runtimeCaching: [{
+                urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
+                handler: 'CacheFirst',
+
+                options: {
+                    cacheName: 'images',
+                    expiration: {
+                        maxEntries: 10,
+                    },
+                },
+            }],
+        }),
+        // new HtmlWebpackPlugin({
+        //     template: '404.html'
+        // })
     ]
 };
--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,61 +1,16 @@
 import Typewriter from 'typewriter-effect/dist/core';
 import { paths, globalConstants } from './../constants/';
-// import * as serviceWorker from './workers/service-worker';
-import './../styles/main.scss';
-import './../styles/content.scss';
-import './../styles/navigation.scss';
+import './../styles/global.scss';
 
 if ('serviceWorker' in navigator) {
-    /**
-     * Define if <link rel='next|prev|prefetch'> should
-     * be preloaded when accessing this page
-     */
-    const PREFETCH = true;
-
-    /**
-     * Define which link-rel's should be preloaded if enabled.
-     */
-    const PREFETCH_LINK_RELS = ['index', 'next', 'prev', 'prefetch'];
-
-    /**
-     * prefetchCache
-     */
-    function prefetchCache() {
-        if (navigator.serviceWorker.controller) {
-            let links = document.querySelectorAll(
-                PREFETCH_LINK_RELS.map(rel => {
-                    return 'link[rel=' + rel + ']';
-                }).join(',')
-            );
-
-            if (links.length > 0) {
-                Array.from(links).map(link => {
-                    let href = link.getAttribute('href');
-                    navigator.serviceWorker.controller.postMessage({
-                        action: 'cache',
-                        url: href,
-                    });
-                });
-            }
-        }
-    }
-    /**
-     * Register Service Worker
-     */
-    navigator.serviceWorker.register('service-worker.js').then(() => {
-        console.log('Service Worker Registered');
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/service-worker.js').then(registration => {
+        console.log('SW registered: ', registration);
+      }).catch(registrationError => {
+        console.log('SW registration failed: ', registrationError);
+      });
     });
-
-    /**
-     * Wait if ServiceWorker is ready
-     */
-    navigator.serviceWorker.ready.then(() => {
-        console.log('Service Worker Ready');
-        if (PREFETCH) {
-            prefetchCache();
-        }
-    });
-}
+  }
 
 const words = [
     'Cześć.',
@@ -69,16 +24,9 @@ const words = [
     'Bonjour.',
     'Hallå.',
 ].map(word => word.toUpperCase());
+
 new Typewriter('#typewriter', {
     strings: words,
     autoStart: true,
     loop: true,
 });
-
-function handleResponse(status, readyState, responseText) {
-    if (readyState == 4 && status == 200) {
-        aboutMeContainer.innerText = responseText;
-    } else if (readyState == 4 && status == 404) {
-        aboutMeContainer.innerText = globalConstants.NOT_FOUND;
-    }
-}
