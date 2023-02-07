b35a3a240f501d0f2d96412aa32c1e96167fdbf1
carlst3 <carlst3@gmail.com>
2023-02-07 08:27:43 -0500

    chore: bump packages, remove no longer needed ones

--- next.config.js
+++ next.config.js
@@ -1,21 +1,8 @@
-const withOffline = require('next-offline');
 const withSvgr = require("next-svgr");
 const { i18n } = require('./next-i18next.config')
 
 const nextConfig = {
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    // Note: we provide webpack above so you should not `require` it
-    // Perform customizations to webpack config
-    // Important: return the modified config
-    config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//));
-    return config;
-  },
-  webpackDevMiddleware: config => {
-    // Perform customizations to webpack dev middleware config
-    // Important: return the modified config
-    return config;
-  },
   i18n,
 };
 
-module.exports = withSvgr(withOffline(nextConfig));
+module.exports = withSvgr(nextConfig);
